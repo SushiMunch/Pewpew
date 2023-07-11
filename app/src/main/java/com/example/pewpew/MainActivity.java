7cfbb061888ae3ff98c7a2cb262d25d3ba9fb643
SushiMunch <dominic.higa.catabui@gmail.com>
2023-07-11 15:30:41 +0800

    update

--- app/src/main/java/com/example/pewpew/MainActivity.java
+++ app/src/main/java/com/example/pewpew/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.pewpew;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import android.os.Bundle;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        setContentView(new Game (this));
     }
 }
